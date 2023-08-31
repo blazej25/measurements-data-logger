5082112ce98120e307574a6addff38358ece8df1
SzymonKubica <szymo.kubica@gmail.com>
2023-08-31 22:24:40 +0100

    Started working on the aspiration screen.

--- screens/aspiration.py
+++ screens/aspiration.py
@@ -1,6 +1,15 @@
 from kivy.app import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.properties import StringProperty, ObjectProperty, ListProperty
 
 class AspirationScreen(Screen):
     Builder.load_file('screens/aspiration.kv')
+
+    number_of_measurements = StringProperty(None)
+
+    def print(self):
+        print(number_of_measurements)
+
+
+
 
