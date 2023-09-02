a1bb7f454f9357e7b11cff83bde994d6fd0a36b6
bkubica25 <bkubica25@gmail.com>
2023-09-02 21:40:38 +0200

    Started working on h2o screen

--- screens/h2o.py
+++ screens/h2o.py
@@ -1,6 +1,15 @@
-
 from kivy.app import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.properties import StringProperty, ListProperty 
 
 class H2OScreen(Screen):
     Builder.load_file('screens/h2o.kv')
+
+    number_of_measurements = StringProperty()
+
+    def on_text_print(self):
+        print(self.number_of_measurements)
